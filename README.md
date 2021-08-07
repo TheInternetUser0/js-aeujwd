cc1f3d4dfb16c33c97360752a29c0e62082e8e78
TheInternetUser0 <beabgamer@gmail.com>
2021-08-07 22:23:55 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-aeujwd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-aeujwd)
---
+++ index.js
@@ -0,0 +1,29 @@
+// Import stylesheets
+import './style.css';
+
+// let num1 = prompt('First number');
+// let num2 = prompt('Second number');
+// num1 = Number(num1);
+// num2 = Number(num2);
+let num1 = 69
+let num2 = 96
+
+function add() {
+  let result = num1 + num2;
+  console.log(result);
+}
+
+function subtract() {
+  let result = num1 - num2;
+  console.log(result);
+}
+
+function multiply() {
+  let result = num1 * num2;
+  console.log(result);
+}
+
+function divide() {
+  let result = num1 / num2;
+  console.log(result);
+}

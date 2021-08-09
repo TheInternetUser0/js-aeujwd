21104612dc219b3a11ff396a02af4089766c87f6
TheInternetUser0 <beabgamer@gmail.com>
2021-08-09 13:29:23 +0300

    1 file modified

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-// Import stylesheets
-import './style.css';
 
-// let num1 = prompt('First number');
-// let num2 = prompt('Second number');
-// num1 = Number(num1);
-// num2 = Number(num2);
 let num1 = 69
 let num2 = 96
 
